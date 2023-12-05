38b36f940d2dac72b7da14e68d5e5d7f487a69a1
WillHalimLee <leehalim10@gmail.com>
2023-12-04 19:20:57 -0800

    changed cs.sh and added test output

--- SAAF (Will)/callservice.sh
+++ SAAF (Will)/callservice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # JSON object to pass to Lambda Function
-json={"\"bucketname\"":\"test.bucket.462562f23.aaa\"","\"filename\"":\"test.csv\""}
+json={"\"bucketname\"":\"462projectbucket\"","\"filename\"":\"test.csv\""}
 echo "Invoking Lambda function using AWS CLI"
 time output=`aws lambda invoke --invocation-type RequestResponse --function-name Service_Two --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
 echo ""
