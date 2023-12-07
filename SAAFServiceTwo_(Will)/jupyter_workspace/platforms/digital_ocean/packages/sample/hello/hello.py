ae784723047298effb1f7e6620b785c2a6498f43
WillHalimLee <leehalim10@gmail.com>
2023-12-07 02:11:48 -0800

    Finished s3, cleaned up s1 and s2

--- SAAFServiceTwo_(Will)/jupyter_workspace/platforms/digital_ocean/packages/sample/hello/hello.py
+++
@@ -1,5 +0,0 @@
-import handler
-import json
-
-def main(args):
-      return {"body": handler.yourFunction(args, None)}
--- SAAFServiceTwo_(Will)/jupyter_workspace/platforms/local/local_runner.py
+++
@@ -1,7 +0,0 @@
-import handler
-import json
-import sys
-
-request = json.loads(sys.argv[1])
-
-print(json.dumps(handler.yourFunction(request, {})))
